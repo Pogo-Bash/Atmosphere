e49425ffc556020c99ee4a07a0c4eea43ccdee96
Pogo-Bash <swapnanildasgupta9@gmail.com>
2023-04-13 19:40:28 -0500

    fixed backgorund js and renamed

---
+++ Atmosphere/background.js
@@ -0,0 +1,28 @@
+console.log("Background script loaded");
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Retrieve the list of blocked URLs from storage
+  chrome.storage.sync.get("blockedUrls", function(result) {
+    var blockedUrls = result.blockedUrls || [];
+
+    // Listen for web requests and block the ones that match the blocked URLs
+    chrome.webRequest.onBeforeRequest.addListener(
+      function(details) {
+        if (blockedUrls.some(function(url) { return details.url.indexOf(url) !== -1 })) {
+          return { cancel: true };
+        }
+      },
+      { urls: ["<all_urls>"] },
+      ["blocking"]
+    );
+  });
+});
+
+// Listen for changes to the blocked URL list and update the storage accordingly
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "updateBlockedUrls") {
+    chrome.storage.sync.set({ blockedUrls: request.blockedUrls }, function() {
+      sendResponse();
+    });
+  }
+});
